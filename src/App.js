b33bbe724cdadd58cddae816e2cfb1243a247769
nikita.kostenkov <nkostenkov@gotrg.com>
2018-05-08 13:15:04 +0300

    add redux

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setBooks } from './actions/books';
+
+class App extends Component {
+  render() {
+    const { books } = this.props.books;
+    const { setBooks } = this.props;
+    const newBooks = [
+        {
+            id: 0,
+            title: 'World'
+        }
+    ];
+    return (
+      <div className='container'>
+          <h1>{books[0].title}</h1>
+        <button onClick={setBooks.bind(this, newBooks)}>button</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+    ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+    setBooks: books => dispatch(setBooks(books))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
